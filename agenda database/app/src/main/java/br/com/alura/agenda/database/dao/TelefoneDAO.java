72d01ab207ea4281052024d23f368c1844225a1c
Guilherme Augusto da Rocha Manhani <gmanhani@kacielis-mini.unicred-parana.corp>
2020-02-17 17:18:41 -0300

    :rocket: implementado funcao de editar e mostrar numero

--- agenda database/app/src/main/java/br/com/alura/agenda/database/dao/TelefoneDAO.java
+++ agenda database/app/src/main/java/br/com/alura/agenda/database/dao/TelefoneDAO.java
@@ -2,7 +2,11 @@ package br.com.alura.agenda.database.dao;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 import br.com.alura.agenda.model.Telefone;
 
@@ -14,4 +18,11 @@ public interface TelefoneDAO {
 
     @Insert
     void salva(Telefone... telefones);
+
+    @Query("SELECT * FROM telefone " +
+            "WHERE alunoId = :alunoId")
+    List<Telefone> buscaTodosTelefonesDoAluno(int alunoId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void atualiza(Telefone... telefones);
 }
--- agenda database/app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ agenda database/app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.util.List;
+
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.database.AgendaDatabase;
 import br.com.alura.agenda.database.dao.AlunoDAO;
@@ -29,6 +31,7 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     private AlunoDAO alunoDAO;
     private Aluno aluno;
     private TelefoneDAO telefoneDAO;
+    private List<Telefone> telefonesDoAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,16 +74,35 @@ public class FormularioAlunoActivity extends AppCompatActivity {
 
     private void preencheCampos() {
         campoNome.setText(aluno.getNome());
-//        campoSobrenome.setText(aluno.getSobrenome());
-//        campoTelefoneFixo.setText(aluno.getTelefoneFixo());
         campoEmail.setText(aluno.getEmail());
-//        campoTelefoneCelular.setText(aluno.getTelefoneCelular());
+        telefonesDoAluno = telefoneDAO.buscaTodosTelefonesDoAluno(aluno.getId());
+        for (Telefone telefone: telefonesDoAluno) {
+            if(telefone.getTipo() == TipoTelefone.FIXO){
+                campoTelefoneFixo.setText(telefone.getNumero());
+            } else {
+                campoTelefoneCelular.setText(telefone.getNumero());
+            }
+        }
     }
 
     private void finalizaFormulario() {
         preencheAluno();
         if (aluno.temIdValido()) {
             alunoDAO.edita(aluno);
+
+            String numeroFixo = campoTelefoneFixo.getText().toString();
+            Telefone telefoneFixo = new Telefone(numeroFixo, TipoTelefone.FIXO, aluno.getId());
+            String numeroCelular = campoTelefoneCelular.getText().toString();
+            Telefone telefoneCelular = new Telefone(numeroCelular, TipoTelefone.CELULAR, aluno.getId());
+
+            for (Telefone telefone: telefonesDoAluno) {
+                if(telefone.getTipo() == TipoTelefone.FIXO){
+                    telefoneFixo.setId(telefone.getId());
+                } else {
+                    telefoneCelular.setId(telefone.getId());
+                }
+                telefoneDAO.atualiza(telefoneFixo, telefoneCelular);
+            }
         } else {
            int alunoId = alunoDAO.salva(aluno).intValue();
            String numeroFixo = campoTelefoneFixo.getText().toString();
